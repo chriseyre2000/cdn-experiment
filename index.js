a067adac7b8378c6ca0238ff0e6ced7baa1182e9
Christopher Eyre <christopher.eyre@codurance.com>
2019-12-24 14:02:49 +0000

    Added some cache headers

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/now', (req, res) => res.send(`${Date. now()}`))
+
+app.get('/now-nocache', (req, res) => {
+  res.append('Cache-control', 'private, max-age=0, no-cache')
+  res.send(`${Date. now()}`)
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
